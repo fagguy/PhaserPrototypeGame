dd239c842323201e195de48902c4e77674f9ba9c
fagguy <lim.isaiah@gmail.com>
2017-03-06 22:28:33 +0800

    Initializing repository
    -Added phaser and typescript-collections
    -Created my own TypeScript declaration file for typescript-collections
    so that compiler can compile to single output .js file
    -Modified .gitignore file to ignore TypeScript build files

---
+++ PhaserPrototypeGame/game.ts
@@ -0,0 +1,18 @@
+﻿class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('logo', 'Graphics/phaser2.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}
